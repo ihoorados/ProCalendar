61ba41d5b794f0422bef79f8492a478b3001f620
Hoorad Ramezani <hooradramezani@gmail.com>
2019-10-01 18:25:21 +0330

    Add Calendar Type

---
+++ ProCalendar/Pro Calendar/CalendarType.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarType.swift
+//  ProCalendar
+//
+//  Created by Hoorad on 10/1/19.
+//  Copyright © 2019 Hoorad. All rights reserved.
+//
+
+import Foundation
+
+enum proCalendarType {
+    case monthly
+    case weekly
+    case daily
+}
---
+++ ProCalendar/Pro Calendar/ProCalendarView.swift
@@ -0,0 +1,12 @@
+//
+//  ProCalendarView.swift
+//  ProCalendar
+//
+//  Created by Hoorad on 10/1/19.
+//  Copyright © 2019 Hoorad. All rights reserved.
+//
+
+import Foundation
+
+
+// Mark: -Setup proCalendar Configuration
